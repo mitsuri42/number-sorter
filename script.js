2cc85fb5185231568069622bcae2abbb240f3230
Dmitriy Pysarchuk <dmit.pysarchuk@gmail.com>
2024-04-02 21:08:50 +0300

    Add updateUI function

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ const sortInputArray = (event) => {
   event.preventDefault();
 
   const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value));
+
+  updateUI(inputValues);
+};
+
+const updateUI = (array = []) => {
+  array.forEach((num, i) => {
+    const outputValueNode = document.getElementById(`output-value-${i}`);
+    outputValueNode.innerText = num;
+  });
 };
 
 sortButton.addEventListener("click", sortInputArray);
