38c1b7b3156c2c47d54acaa633ea53a038df0d95
Dmitriy Pysarchuk <dmit.pysarchuk@gmail.com>
2024-04-05 10:33:55 +0300

    Add swap algorithm, remove console log

--- script.js
+++ script.js
@@ -36,12 +36,17 @@ const selectionSort = (array) => {
     let minIndex = i;
 
     for (let j = i + 1; j < array.length; j++) {
-      console.log(array, array[j], array[minIndex]);
       if (array[j] < array[minIndex]) {
         minIndex = j;
       }
     }
+
+    const temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
   }
+
+  return array;
 };
 
 sortButton.addEventListener("click", sortInputArray);
